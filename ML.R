a031ee6a9134cc1fe8d3bff5a049d562e637616c
jeon jong-june <jj.jeon@gmail.com>
2019-07-03 09:25:56 +0900

    ranking
    rank 와  inverse rank  고쳐야 함.

--- ML.R
+++ ML.R
@@ -13,8 +13,6 @@ z = mvrnorm(n, mu, Sigma)
 # pi_mat denotes the ranks of items
 pi_mat =t(apply(z, 1, order, decreasing = T))
 rankIndex_list = rankIndex_fun(pi_mat)
-rank_id = rankIndex_list$rank_id
-rank_index = rankIndex_list$rank_index
 
 # initialization:: pairwise comparisons (package)
 fit_mat = convToMat_fun(pi_mat)
@@ -29,64 +27,75 @@ Omg_e = solve(Sig_e)
 
 
 # sampling function
-i = 2 ; length(rank_id)
-idx = which(rank_index == i)
-x = pi_mat[idx[1],]
-
-# set an initial of z
-while(T)
-{
-  z = mvrnorm(1, mu_e, Sig_e)  
-  zo = order(z, decreasing = T)
-  if (!any(x != zo)) 
-  {
-    if (verbose) cat("Initialization is complete! ")
-    break
-  }
-}
-
-
 burn_num = 1e+4
 restore_num = 5e+3
-# burning
-# ix[j_c] : the index of item with rank j_c
-# iterates according to ranks (j_c from 1 to p)
-j_c = 1
-ix = sort(x, index.return = T)$ix
-for (j in rep(ix, burn_num))
+p = ncol(pi_mat)
+rank_id = rankIndex_list$rank_id
+rank_index = rankIndex_list$rank_index
+zmat_mean = matrix(0,length(rank_id),p)
+for (i in 1:length(rank_id))
 {
-  m = mu_e[j]- sum(Omg_e[j,][-j]*(z[-j] - mu_e[-j]))/Omg_e[j,j]
-  v = sqrt(1/Omg_e[j,j])
-  if (j_c == 1) b = Inf else b = z[ix[j_c-1]]
-  if (j_c == p) a = -Inf else a = z[ix[j_c+1]]
-  z[j] = rtruncnorm(1, a=a, b=b, mean = m, sd = v)
-  j_c = j_c + 1
-  if (j_c>p) j_c = 1
-}
-
-# restore
-
-j_k = 1
-j_c = 1
-zmat = matrix(0, restore_num, p)
-for (j in rep(ix, restore_num))
-{
-  m = mu_e[j]- sum(Omg_e[j,][-j]*(z[-j] - mu_e[-j]))/Omg_e[j,j]
-  v = sqrt(1/Omg_e[j,j])
-  if (j_c == 1) b = Inf else b = z[ix[j_c-1]]
-  if (j_c == p) a = -Inf else a = z[ix[j_c+1]]
-  tz = rtruncnorm(1, a=a, b=b, mean = m, sd = v)
-  z[j] = tz
-  j_c = j_c + 1
-  if (j_c>p)
+  # 
+  cat("#iter: ", i, '\n')
+  idx = which(rank_index == i)
+  x = pi_mat[idx[1],]
+  # set an initial of z
+  while(T)
   {
-    zmat[j_k,] = z
-    j_c = 1
-    j_k = j_k + 1
+    z = mvrnorm(1, mu_e, Sig_e)  
+    zo = order(z, decreasing = T)
+    if (!any(x != zo)) 
+    {
+      if (verbose) cat("Initialization is complete! ")
+      break
+    }
   }
+  # burning
+  # ix[j_c] : the index of item with rank j_c
+  # iterates according to ranks (j_c from 1 to p)
+  j_c = 1
+  ix = sort(x, index.return = T)$ix
+  for (j in rep(ix, burn_num))
+  {
+    m = mu_e[j]- sum(Omg_e[j,][-j]*(z[-j] - mu_e[-j]))/Omg_e[j,j]
+    v = sqrt(1/Omg_e[j,j])
+    if (j_c == 1) b = Inf else b = z[ix[j_c-1]]
+    if (j_c == p) a = -Inf else a = z[ix[j_c+1]]
+    if (a>=b) stop("error 1")
+    z[j] = rtruncnorm(1, a=a, b=b, mean = m, sd = v)
+    if (is.na(z[j])) stop("NA occurs")
+    j_c = j_c + 1
+    if (j_c>p) j_c = 1
+  }
+  
+  # restore
+  j_k = 1
+  j_c = 1
+  zmat = matrix(0, restore_num, p)
+  for (j in rep(ix, restore_num))
+  {
+    m = mu_e[j]- sum(Omg_e[j,][-j]*(z[-j] - mu_e[-j]))/Omg_e[j,j]
+    v = sqrt(1/Omg_e[j,j])
+    if (j_c == 1) b = Inf else b = z[ix[j_c-1]]
+    if (j_c == p) a = -Inf else a = z[ix[j_c+1]]
+    tz = rtruncnorm(1, a=a, b=b, mean = m, sd = v)
+    z[j] = tz
+    j_c = j_c + 1
+    if (j_c>p)
+    {
+      zmat[j_k,] = z
+      j_c = 1
+      j_k = j_k + 1
+    }
+  }
+  
+  zmat_mean[i,] = colMeans(zmat)
 }
 
-colMeans(zmat)
+
+
+
+
 boxplot(zmat)
 
 #while(T)
