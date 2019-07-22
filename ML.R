20ba45918cf7c456e8d9cf8efb6e82523f77d510
jeon jong-june <jj.jeon@gmail.com>
2019-07-22 22:14:08 +0900

    parallel
    Add the parallel processing code
    병렬처리 부분 추가

--- ML.R
+++ ML.R
@@ -1,5 +1,7 @@
 rm(list = ls()); gc()
 library(MASS)
+library(doParallel)
+library(foreach)
 if (Sys.info()[1]=="Linux")
 {
   setwd("~/Documents/GitHub/MLE_Thurstone")
@@ -9,18 +11,23 @@ if (Sys.info()[1]=="Linux")
 
 if (!require(truncnorm)) {install.packages("truncnorm") ; library(truncnorm)}
 source("./lib/conv.R")
-source("./lib/em_lib.R")
-p = 3
+source("./lib/em_lib.R")  
+
+numCores = 8
+registerDoParallel(numCores)  # use multicore, set to the number of our cores
+
+p = 5
 mu = seq(2,0, length = p)
 Sigma = matrix(0.5, p, p)
 Sigma[,p] =  Sigma[p,] = 0
 diag(Sigma) = 1
 
-n = 100
+n = 1000
 burn_num = 1e+2
 restore_num = 5e+3
+parallel = T
 verbose = F
-#set.seed(1)
+set.seed(1)
 z = mvrnorm(n, mu, Sigma)
 # pi_mat denotes the index of items
 # pi_mat[i,k]: the index of item with rank k in the ith experi
@@ -39,21 +46,24 @@ mu_e = c(fit_model$coefficients*sqrt(2),0) # note: probit reg z~N(0, 1/sqrt(2))
 Sig_e = diag(1,p)
 Omg_e = solve(Sig_e)
 # sampling function
-for (iter in 1:1000)
-{
-  cat("outer iter:", iter ,'\n')
-  E_fit = E_fun.prob(rankIndex_list, mu_e, Sig_e, Omg_e,
-                     burn_num, restore_num, verbose)
-  ##
-  mu_e = E_fit$Ez
-  Sig_e = E_fit$Covz
-  Sig_e = Sig_e - mu_e%*%t(mu_e)
-  Sig_e[1,-1] = Sig_e[1,-1]/sqrt(Sig_e[1,1])
-  Sig_e[-1,1] = Sig_e[-1,1]/sqrt(Sig_e[1,1])
-  Sig_e[1,1] = 1
-  Omg_e = solve(Sig_e)
-  cat("me:", mu_e, '\n')
-  cat("Frobenius norm:", sum((Sigma-Sig_e)^2), '\n')
-  print(Sig_e)
+system.time({
+  for (iter in 1:4)
+  {
+    cat("outer iter:", iter ,'\n')
+    E_fit = E_fun.prob(pi_mat, rankIndex_list, mu_e, Sig_e, Omg_e,
+                       burn_num, restore_num, parallel, verbose)
+    ##
+    mu_e = E_fit$Ez
+    Sig_e = E_fit$Covz
+    Sig_e = Sig_e - mu_e%*%t(mu_e)
+    Sig_e[1,-1] = Sig_e[1,-1]/sqrt(Sig_e[1,1])
+    Sig_e[-1,1] = Sig_e[-1,1]/sqrt(Sig_e[1,1])
+    Sig_e[1,1] = 1
+    Omg_e = solve(Sig_e)
+    cat("me:", mu_e, '\n')
+    cat("Frobenius norm:", sum((Sigma-Sig_e)^2), '\n')
+    print(Sig_e)
+  }
 }
+)
 
